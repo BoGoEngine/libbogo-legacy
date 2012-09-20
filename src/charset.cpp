731c9ea63a609b6658ceffe7a6b68844f2fecd3b
Duong Ha Quang <contact@haqduong.net>
2012-09-20 19:23:42 +0700

    Implement converting from utf8 to TCVN3

--- src/charset.cpp
+++ src/charset.cpp
@@ -1,9 +1,72 @@
+/*
+
+  This file is a part of BoGoEngine project.
+
+  Copyright (C) 2012 Dương H. Nguyễn <cmpitg@gmail.com>
+  Copyright (C) 2012 Dương Quang Hà <contact@haqduong.net>
+
+  BoGoEngine is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  BoGoEngine is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with BoGoEngine.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include "charset.hpp"
 
 namespace BoGo {
 
     using namespace Glib;
 
-    void utf8toCharset (ustring src, const gchar *dest, CharSetT charset) {
+#ifdef _
+#undef _
+#endif
+#define _(x) (ustring ("") + x)
+
+#ifdef __
+#undef __
+#endif
+#define __(x) (ustring ("") + x).c_str ()
+
+
+    // void utf8toCharset (ustring src, const gchar *dest, CharSetT charset) {
+    //     ustring result = ustring("");
+    //     for (_size_t_ i = 0; i < src.length (); i++) {
+    //         _size_t_ pos = UTF8_CHARS.find (src[i]);
+    //         if (pos != ustring::npos)
+    //             result += _(utf8ToTCVN3[pos]);
+    //         else
+    //             result += src[i];
+    //     }
+    //     return result;
+    // }
+
+    const gchar * utf8toCharset (ustring src, const gchar *dest, CharSetT charset) {
+        ustring result = ustring("");
+
+        const unsigned int * destCharset;
+
+        if (charset == CHARSET_TCVN3) {
+            destCharset = utf8ToTCVN3;
+        }
+
+        for (_size_t_ i = 0; i < src.length (); i++) {
+            _size_t_ pos = UTF8_CHARS.find (src[i]);
+            if (pos != ustring::npos)
+                result += _(destCharset[pos]);
+            else
+                result += src[i];
+        }
+        return result.c_str ();
     }
+
+
 }
---
+++ test/test_charset.cpp
@@ -0,0 +1,45 @@
+/*
+
+ This file is a part of BoGoEngine project.
+
+ Copyright (C) 2012 Dương H. Nguyễn <cmpitg@gmail.com>
+ Copyright (C) 2012 Dương Quang Hà <contact@haqduong.net>
+
+ BoGoEngine is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ BoGoEngine is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with BoGoEngine. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <charset.hpp>
+
+using namespace std;
+using namespace Glib;
+using namespace BoGo;
+
+#define _(s) (ustring ("") + s)
+#define __(s) (ustring ("") + s).c_str ()
+
+TEST (EncodingConverter, UTF8ToTCVN3) {
+    EXPECT_STREQ ("aA¨¡©¢bBcCdD®§eEª£fFgGhHiIjJkKlLmMnNoO«¤¬¥pPqQrRsStTuU­¦vVwWxXyYzZ",
+                  utf8toCharset (_("aAăĂâÂbBcCdDđĐeEêÊfFgGhHiIjJkKlLmMnNoOôÔơƠpPqQrRsStTuUưƯvVwWxXyYzZ"),
+                                 "", CHARSET_TCVN3));
+}
+
+int main (int argc, char *argv[]) {
+    testing::InitGoogleTest (&argc, argv);
+    int dummyVal = RUN_ALL_TESTS ();
+
+    return 0;
+}
